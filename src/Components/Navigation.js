373346c043732834705e8dbf32b080aaf4f63aa9
Precious <Preshomondi@gmail.com>
2023-06-20 23:57:46 +0300

    Add a search bar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,33 +1,42 @@
-import React from 'react'
-import './Navigation.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faCartShopping, faCoffee } from '@fortawesome/free-solid-svg-icons'
-  
+import React from "react";
+import "./Navigation.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 function Navigation(props) {
   return (
     <nav>
-        <ul>
-            {props.isLoggedIn && (
-                <li>
-                   <img src={"/assets/background.jpeg"} className="nav-avatar"/>
-                </li>
-            )}
-            {props.isLoggedIn && (
-                <li>
-                    <a href="/">
-                        <FontAwesomeIcon icon={faCartShopping} className="nav-icon"/>
-                    </a>
-                </li>
-            )}
-            {props.isLoggedIn && (
-                <li>
-                    <button onClick={props.onLogout}>Logout</button>
-                </li>
-            )}
-        </ul>
+      <ul>
+        {props.isLoggedIn && (
+          <div className="searchbar ui segment">
+              <form className="ui form">
+                  <div className="field">
+                     <input type="text"/> 
+                  </div>
+              </form>
+           
+          </div>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <img src={"/assets/background.jpeg"} className="nav-avatar" />
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">
+              <FontAwesomeIcon icon={faCartShopping} className="nav-icon" />
+            </a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
