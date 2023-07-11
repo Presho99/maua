420c5202b895fc7c782c4381dea2daf995496a9a
Precious <Preshomondi@gmail.com>
2023-07-11 15:55:19 +0300

    Wrap with cart provider

--- src/App.js
+++ src/App.js
@@ -1,38 +1,62 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from "react";
 import Home from "./Components/Home";
-
+import Loader from "./Loader";
 import Login from "./Components/Login";
-import MainHeader from './Components/MainHeader';
+import MainHeader from "./Components/MainHeader";
+import CartProvider from "./store/CartProvider";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
-    setIsLoggedIn(true)
-  }
+    setIsLoggedIn(true);
+  };
 
   const logoutHandler = () => {
-    setIsLoggedIn(false)
-  }
+    setIsLoggedIn(false);
+  };
 
-   // For the cart
-   const [cartIsShown, setCartIsShown] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+  // For the cart
+  const [cartIsShown, setCartIsShown] = useState(true);
 
-   const showCartHandler = () => {
-     setCartIsShown(true)
-   }
- 
-   const hideCartHandler = () => {
-     setCartIsShown(false)
-   }
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <div classname="app">
-     <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} showCartHandler={showCartHandler} />
-     <main>
-       {!isLoggedIn && <Login onLogin = {loginHandler}/>}
-       {isLoggedIn && <Home onLogout={logoutHandler} cartIsShown={cartIsShown} hideCartHandler={hideCartHandler}/>}
-     </main>
-    </div>
+    <CartProvider classname="app">
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <MainHeader
+            isAuthenticated={isLoggedIn}
+            onLogout={logoutHandler}
+            showCartHandler={showCartHandler}
+          />
+          <main>
+            {!isLoggedIn && <Login onLogin={loginHandler} />}
+            {isLoggedIn && (
+              <Home
+                onLogout={logoutHandler}
+                cartIsShown={cartIsShown}
+                hideCartHandler={hideCartHandler}
+              />
+            )}
+          </main>
+        </div>
+      )}
+    </CartProvider>
   );
 }
 
