0d19d50a59ffbbe3438b1c978fea8312c517475e
Precious <Preshomondi@gmail.com>
2023-07-17 21:36:29 +0300

    restyle login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,7 +9,7 @@ function Login(props) {
   const [password, setPassword] = useState("");
   const [validPassword, setValidPassword] = useState();
   const [validForm, setValidForm] = useState(false);
-  const [error, setError] = useState()
+  const [error, setError] = useState();
 
   const emailChangeHandler = (e) => {
     setEnteredEmail(e.target.value);
@@ -32,65 +32,70 @@ function Login(props) {
   };
 
   const submitHandler = (e) => {
-    e.preventDefault()
-    if(enteredEmail.trim().length === 0 || password.trim().length === 0){
+    e.preventDefault();
+    if (enteredEmail.trim().length === 0 || password.trim().length === 0) {
       setError({
-        title: 'Invalid input',
-        message: 'Please enter a valid email address and password (non-empty values)'
-      })
-    }else if(password.trim().length <8){
+        title: "Invalid input",
+        message:
+          "Please enter a valid email address and password (non-empty values)",
+      });
+    } else if (password.trim().length < 8) {
       setError({
-        title: 'Invalid password',
-        message: 'Password should be at least 8 characters'
-      })
-    }else 
-    props.onLogin(enteredEmail, password)
-  }
+        title: "Invalid password",
+        message: "Password should be at least 8 characters",
+      });
+    } else props.onLogin(enteredEmail, password);
+  };
 
   return (
     <div>
-    {error && <ErrorModal title={error.title} message={error.message}/>}
-    <div className="login" style={{ backgroundImage: `url(/assets/background.jpeg)` }}>
-      <div className="login-container">
-        <div className="login-image">
-          <img src={"/assets/background.jpeg"} />
-        </div>
-        <div className= "login-name">
-          <h3>Maua</h3>
-        </div>
-        <form onSubmit={submitHandler}>
-          <div className="email">
-            <input
-              type="email"
-              id="email"
-              onChange={emailChangeHandler}
-              value={enteredEmail}
-              onBlur={validateEmailHandler}
-            />
+      {error && <ErrorModal title={error.title} message={error.message} />}
+      <div
+        className="login"
+              >
+        <div className="login-container">
+          <div className="login-image">
+            <img src={"/assets/background.jpeg"} />
           </div>
+          {/* <div className="login-name">
+            <h3>Maua</h3>
+          </div> */}
+          <form onSubmit={submitHandler}>
+            <div className="email">
+              <input
+                type="email"
+                id="email"
+                placeholder="Email"
+                onChange={emailChangeHandler}
+                value={enteredEmail}
+                onBlur={validateEmailHandler}
+              />
+            </div>
 
-          <div className="password">
-            <input
-              type="password"
-              id="password"
-              onChange={passwordChangeHandler}
-              value={password}
-              onBlur={validatePasswordHandler}
-            />
-          </div>
-          <div className="login-links">
-            <a href="#">Forgot Password</a>
-            <a href="#">Signup</a>
-          </div>
+            <div className="password">
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                onChange={passwordChangeHandler}
+                value={password}
+                onBlur={validatePasswordHandler}
+              />
+            </div>
+           
 
-          <div className="login-button">
-            <Button type="submit" className="btn-login" disabled={!validForm}>
-              Login
-            </Button>
-          </div>
-        </form>
+            <div className="login-button">
+              <Button type="submit" className="btn-login" disabled={!validForm}>
+                Login
+              </Button>
+            </div>
+            <div className="login-links">
+              <a href="#">Forgot Password</a>
+              <a href="#">Signup</a>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
